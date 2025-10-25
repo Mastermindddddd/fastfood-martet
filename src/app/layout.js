ad0f50a8354a64f3e1e876c2139712a639dcb931
Mastermindddddd <malusicephas17@gmail.com>
2025-10-25 09:21:54 +0200

    ...

--- src/app/layout.js
+++ src/app/layout.js
@@ -86,7 +86,7 @@ export default function RootLayout({ children }) {
           </div>
         </div>
       </footer>
-            <LocationFooterBar />
+            {/*<LocationFooterBar />*/}
           </AppProvider>
         </main>
       </body>
